44a03f3868a2f71b2bc1ac1ff3d9e69c6b0a934c
JpNery_Tech <jppereiranery@gmail.com>
2025-05-02 09:15:12 -0300

    Delete tipos_de_dados directory

--- tipos_de_dados/9_exercicio_5/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-$nome = "João Pedro Pereira Nery";
-$idade = 18;
-
-echo "Meu nome é $nome e tenho $idade anos! <br>"; // concatenação com aspas duplas
-
-echo 'Meu nome é ' . $nome . ' e tenho ' . $idade . ' anos!'; // concatenação com aspas simples
